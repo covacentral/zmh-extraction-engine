7750a6a7073d403182ceaef7a4a60c38ea984865
Daniel Robles <covacentral@gmail.com>
2026-05-10 12:57:42 -0500

    Chore: Remove debug scripts and scratch files from repo

--- test-catalog.js
+++
@@ -1,2 +0,0 @@
-const { default: makeWASocket } = require('@whiskeysockets/baileys');
-console.log(Object.keys(makeWASocket({}).constructor.prototype || {}).filter(k => k.toLowerCase().includes('catalog')));
